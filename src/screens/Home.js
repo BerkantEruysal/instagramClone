05df51637d7c690469ad45ad9e008d1ce75d39ed
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-01-28 23:40:18 +0300

    Add story section to home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,11 +3,13 @@ import React from 'react';
 import HomeHeader from '../components/HomeHeader';
 import Footer from '../components/Footer';
 import HomeMainContent from '../components/HomeMainContent';
+import Stories from '../components/Stories';
 
 export default function Home() {
   return (
     <View>
       <HomeHeader></HomeHeader>
+      <Stories></Stories>
       <HomeMainContent></HomeMainContent>
       <Footer></Footer>
     </View>
