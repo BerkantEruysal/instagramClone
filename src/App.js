f499026915aa834fb07626cd403351c60e4d676e
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-01-28 23:39:36 +0300

    Add redux boilerplate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React , {useState} from 'react';
-import {
-  Button,
-  Text,
-  View,
-} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import Store from './redux/Store';
+import {Provider} from 'react-redux';
 import Authentication from './screens/Authentication';
-import Home from "./screens/Home"
+import Home from './screens/Home';
 
-
-
-
-const App =  () => {
-
+const App = () => {
   return (
     <View>
-     <Home></Home>
+      <Provider store={Store}>
+        <Home></Home>
+      </Provider>
     </View>
   );
 };
 
-
 //This line of code is required in order to use EStyleSheet module.
-EStyleSheet.build()
-
+EStyleSheet.build();
 
 export default App;
