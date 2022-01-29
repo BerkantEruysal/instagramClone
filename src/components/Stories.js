64f97b8e70ac3e6124c4de4bf73a7d63676ea769
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-01-29 22:16:40 +0300

    Add flatlist functionality and create test data

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,25 +1,74 @@
-import { View, Text , Dimensions , StyleSheet} from 'react-native';
+import {View, Text, Dimensions, StyleSheet, FlatList} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import React from 'react';
 import Story from './Story';
-
+const DATA = [
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'asdasd',
+    isWatched: true,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'berkant',
+    isWatched: true,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'asdasd',
+    isWatched: true,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'berkant',
+    isWatched: true,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'asdasd',
+    isWatched: true,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'berkant',
+    isWatched: false,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'asdasd',
+    isWatched: false,
+  },
+  {
+    image: require('../assets/tempProfileIcon.png'),
+    userName: 'berkant',
+    isWatched: false,
+  },
+];
 export default function Stories() {
+  const renderItem = ({item}) => {
+    return (
+      <Story
+        image={item.image}
+        userName={item.userName}
+        isWatched={item.isWatched}></Story>
+    );
+  };
   return (
     <View style={styles.wrapper}>
-      <Story></Story>
-      <Story></Story>
-      <Story></Story>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={DATA}
+        renderItem={renderItem}></FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    wrapper : {
-        height : Dimensions.get("window").height * 12.8 / 100 ,
-        borderBottomColor: 'rgba(38 , 38 , 38 , 0.6)',
-        borderBottomWidth: 0.5,
-        flexDirection : "row"
-
-
-    }
-})
+  wrapper: {
+    height: (Dimensions.get('window').height * 12.8) / 100,
+    borderBottomColor: 'rgba(38 , 38 , 38 , 0.6)',
+    borderBottomWidth: 0.5,
+    flexDirection: 'row',
+  },
+});
