5cd99f901b1960479742f26b28150374d711706d
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-02-15 21:43:12 +0300

    Add encrypted storage functions for access tokens

--- src/api/AuthenticationApi.js
+++ src/api/AuthenticationApi.js
@@ -1,4 +1,5 @@
 import {setTokens} from '../redux/slices/User';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 export const logIn = async (dispatch, {password, userName}) => {
   let tokens = await fetch('http://192.168.20.107:8000/api/token/', {
@@ -14,6 +15,39 @@ export const logIn = async (dispatch, {password, userName}) => {
  
   tokens = await tokens.json();
   dispatch(
-    setTokens({accessToken: tokens.access, refreshToken: tokens.refresh}),
+    setTokens({accessToken: tokens.access, refreshToken: tokens.refresh})
   );
+  setTokensToEncrypetStorage(tokens.access , tokens.refresh)
 };
+
+export const getTokensFromEncryptedStorage = async (dispatch , setIsLoading) => {
+  try {   
+    let session = await EncryptedStorage.getItem("user_session");
+    session = JSON.parse(session)
+    if (session.accessToken !== undefined) {
+      console.log("asd")
+      
+      dispatch(setTokens({accessToken : session.accessToken , refreshToken: session.refresh}))
+        
+    }
+    setIsLoading(false)
+} catch (error) {
+    console.log(error)
+}
+}
+
+export const setTokensToEncrypetStorage = async (access , refresh) => {
+  try {
+    await EncryptedStorage.setItem(
+        "user_session",
+        JSON.stringify({
+            accessToken : access,
+            refreshToken : refresh
+        })
+    );
+
+  
+} catch (error) {
+   
+}
+}
