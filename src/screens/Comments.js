92a9869aae2a420b8a93a6b8bd510ed9b2564ea7
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-02-16 10:19:58 +0300

    Add "like comment" functionalitiy

--- src/screens/Comments.js
+++ src/screens/Comments.js
@@ -1,17 +1,39 @@
-import {View, FlatList} from 'react-native';
+import {View, FlatList , Text} from 'react-native';
 import React from 'react';
 import Comment from '../components/Comment';
-import { useSelector } from 'react-redux';
+import { useSelector , useDispatch } from 'react-redux';
+import { fetchComments } from '../api/PostsApi';
+import { addToComments, resetCommentState , increasePage } from '../redux/slices/Comments';
+import { useEffect } from 'react';
 
 
 
-export default function Comments() {
-  const DATA = useSelector(state => state.Comments)
+export default function Comments({route}) {
+  const dispatch = useDispatch()
+  const {commentList , page } = useSelector(state => state.Comments)
+  
+  const {accessToken} = useSelector(state => state.User)
   const renderItem = ({item}) => {
     return <Comment props={item}></Comment>
   }
+
+  useEffect(() => {
+  
+   
+    getMoreComments()
+  }, [])
+  const getMoreComments = () => {
+    fetchComments(route.params.postId , accessToken , page , (data) => {
+      dispatch(addToComments(data))
+    
+      dispatch(increasePage())
+    
+    })
+  }
   return <View>
-      <FlatList data={DATA} renderItem={renderItem}>
+      <FlatList
+      onEndReached={getMoreComments}
+      data={commentList} renderItem={renderItem}>
       </FlatList>
   </View>;
 }
