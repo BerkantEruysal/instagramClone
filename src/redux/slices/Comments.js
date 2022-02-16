89f8259bd564405cf3b4402c80d0eb73b29fc103
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-02-16 10:22:00 +0300

    Add reducers needed for unlimited scroll and like comment functionality

--- src/redux/slices/Comments.js
+++ src/redux/slices/Comments.js
@@ -1,46 +1,46 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialState = [
-  {
-    userName: 'berkant',
-    commentText: 'deneme deneme',
-    activeStory: true,
-    isStoryWatched: false,
-    timePassed: '10d',
-    numberOfLikes: 12,
-  },
-  {
-    userName: 'berkant',
-    commentText: 'deneme deneme',
-    activeStory: false,
-    isStoryWatched: false,
-    timePassed: '10d',
-    numberOfLikes: 12,
-  },
-  {
-    userName: 'berkant',
-    commentText: 'deneme deneme',
-    activeStory: true,
-    isStoryWatched: false,
-    timePassed: '10d',
-    numberOfLikes: 12,
-  },
-  {
-    userName: 'berkant',
-    commentText: 'deneme deneme',
-    activeStory: true,
-    isStoryWatched: false,
-    timePassed: '10d',
-    numberOfLikes: 12,
-  },
-];
+const initialState = {
+  idOfPost: null,
+  page: 1,
+  commentList: [],
+};
 
 const Comments = createSlice({
   name: 'comments',
   initialState,
-  reducers: {},
+  reducers: {
+    setIdOfPost: (state, {payload}) => {
+      state.idOfPost = payload;
+    },
+    addToComments: (state, {payload}) => {
+      state.commentList = [...state.commentList, ...payload.results];
+    },
+    resetCommentState: state => {
+    
+      state.idOfPost = null
+       state.page = 1 
+       state.commentList = []
+    },
+    changeIsCommentLikedState: (state, {payload}) => {
+      const index = state.commentList.findIndex(
+        obj => obj.unique_id == payload,
+      );
+      if (state.commentList[index].isLiked == true) {
+        state.commentList[index].isLiked = false;
+        state.commentList[index].likeCount -= 1;
+        return;
+      }
+      state.commentList[index].isLiked = true;
+      state.commentList[index].likeCount += 1;
+    },
+    increasePage : (state) => {
+      state.page++
+    }
+  },
 });
 
-export const {} = Comments.actions;
+export const { increasePage , addToComments, setIdOfPost, resetCommentState, changeIsCommentLikedState} =
+  Comments.actions;
 
 export default Comments.reducer;
