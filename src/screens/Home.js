fb763a1d90ce087818da14ec9b2527c2c7d0c9c8
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-02-17 23:14:55 +0300

    Change name of MainContent

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,14 +2,14 @@ import { View, Text , ScrollView } from 'react-native';
 import React from 'react';
 import HomeHeader from '../components/HomeHeader';
 import Footer from '../components/Footer';
-import HomeMainContent from '../components/HomeMainContent';
+import MainContent from '../components/MainContent';
 import Stories from '../components/Stories';
 
 export default function Home({navigation}) {
   return (
     <View>
       <HomeHeader></HomeHeader>
-      <HomeMainContent navigation={navigation} header={Stories}></HomeMainContent>
+      <MainContent navigation={navigation} header={Stories}></MainContent>
       
     </View>
   );
