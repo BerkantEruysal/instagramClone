6a653f3f4632b42ca431c0da4ae43c5f74ff43f9
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-02-22 18:24:15 +0300

    Add Profile Store

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,7 +2,8 @@ import {configureStore} from '@reduxjs/toolkit';
 import HomePosts from './slices/HomePosts';
 import User from './slices/User';
 import Comments from './slices/Comments';
+import Profile from "./slices/Profile"
 
 export default configureStore({
-  reducer: {HomePosts, User, Comments},
+  reducer: {HomePosts, User, Comments , Profile},
 });
