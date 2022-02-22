3449a41101bd8034149094d12b8069dbd899f77b
Berkant Eruysal <BerkantEruysal@hotmail.com>
2022-02-22 18:21:55 +0300

    Create Posts Screen

--- src/screens/Posts.js
+++ src/screens/Posts.js
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import Post from '../components/Post'
 
-export default function Posts() {
+export default function Posts({route , navigation}) {
+  const {postData} = useSelector(state => state.Profile)
+  const {isAnyStory , username , profilePicture} = useSelector(state => state.User)
+  console.log(postData)
+  const renderItem = ({item}) => {
+    return (
+      <Post navigation={navigation} props={{...item , postImage : item.images , user : { isAnyStory , username , profilePicture}}}></Post>
+    )
+  }
   return (
     <View>
-      <Text>Posts</Text>
+      <FlatList
+      initialScrollIndex={route.params.index}
+      renderItem={renderItem}
+      keyExtractor={item => {
+        return item.unique_id
+      }
+        
+        }
+      data={postData}></FlatList>
     </View>
   )
 }
